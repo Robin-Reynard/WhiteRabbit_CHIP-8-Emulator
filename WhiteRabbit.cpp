b9e0655b0abf7ed57bebcfaebf8a11fd59fc1af5
Robin-Reynard <robin.reynard.the.first@gmail.com>
2024-01-24 18:26:18 -0500

    setup main control loop

--- WhiteRabbit.cpp
+++ WhiteRabbit.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-   cout << "Hello C++ im no longer running away";
+   //Setup graphics
+   //Setup input
+
+   //Init chip8
+   //Load game
+
+   while (true){
+    //Emulate cycle
+    // If the draw flag is set, update the screen
+        //Draw graphics
+
+    // Store key press state (Press and Release)
+    //myChip8.setKeys();
+   }
+   
    return 0;
 }
