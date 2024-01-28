472d11763e2685176f9d7429dbcdcb5c0febbbde
Robin-Reynard <robin.reynard.the.first@gmail.com>
2024-01-28 17:26:03 -0500

    create Chip8System object

---
+++ ChipSystem.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class ChipSystem{
+    public:
+        void initialize();
+        void runCycle();
+};
--- WhiteRabbit.cpp
+++ WhiteRabbit.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
+#include "ChipSystem.h"
 using namespace std;
 
 int main() {
+    ChipSystem chip8system;
+
    //Setup graphics
    //Setup input
 
    //Init chip8
+   chip8system.initialize();
    //Load game
 
-   while (true){
+   ///while (true){
     //Emulate cycle
     // If the draw flag is set, update the screen
         //Draw graphics
 
     // Store key press state (Press and Release)
     //myChip8.setKeys();
-   }
+   //}
    
    return 0;
 }
