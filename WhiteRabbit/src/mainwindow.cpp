cb7574a219f01915354eddcfc7ea7088a0101b41
Robin-Reynard <robin.reynard.the.first@gmail.com>
2024-04-09 19:10:58 -0400

    Ditto

--- WhiteRabbit/src/mainwindow.cpp
+++ WhiteRabbit/src/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "chip8.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    
+    ui->label->setText("Hello World\n");
+
+
+    CHIP8 mychip = CHIP8();
+    mychip.load_program();
 }
 
 MainWindow::~MainWindow()
--- WhiteRabbit/src/src.pro
+++ WhiteRabbit/src/src.pro
@@ -16,10 +16,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    chip8.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
+    chip8.h \
     mainwindow.h
 
 FORMS += \
