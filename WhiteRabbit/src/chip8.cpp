ddb0ad55835aee37fd50c4b6a4209782d17be5ce
Robin-Reynard <robin.reynard.the.first@gmail.com>
2024-05-21 22:55:06 -0400

    Plugged Opcode execution logic

--- WhiteRabbit/src/chip8.cpp
+++ WhiteRabbit/src/chip8.cpp
@@ -30,47 +30,102 @@ CHIP8::CHIP8()
 }
 
 void CHIP8::emulate_cycle(){
-    opcode = 0x2001;
-    cout << stack[stack_pointer] << endl;
-    cout << stack_pointer << endl;
-    cout << program_counter << endl;
-    cout << "Executing" << endl;
-    Opcode::execute_2NNN(opcode, program_counter, stack, stack_pointer);
-
-    cout << stack[stack_pointer] << endl;
-    cout << stack_pointer << endl;
-    cout << program_counter << endl;
-    /*print_as_byte(memory[program_counter]);
-    print_as_byte(memory[program_counter + 1]);
     //Fetch opcode
     opcode = memory[program_counter] << 8 | memory[program_counter + 1];
     print_as_byte(opcode);
 
     //Decode opcode
-    switch(opcode & 0xF000){
-    case 0x0000:
-        break;
-    case 0x1000:
-        program_counter = opcode & 0x0FFF;
-        break;
-    case 0x2000:
-        stack[stack_pointer] = program_counter;
-        stack_pointer++;
-        program_counter = opcode & 0x0FFF;
-        break;
 
-    case 0xA000:
-        I = opcode & 0x0FFF;
-        program_counter += 2;
-        break;
-    default:
-        printf ("Unknown opcode: 0x%X\n", opcode);
+    switch(opcode & 0xF000) {
+        case 0x0000:
+            switch(opcode & 0x0FFF){
+                case 0x0E0:
+                    Opcode::execute_00E0(program_counter, graphics); break;
+                default:
+                    Opcode::execute_0NNN(program_counter); break;
+            } break;
+        case 0x1000:
+            Opcode::execute_1NNN(opcode, program_counter); break;
+        case 0x2000:
+            Opcode::execute_2NNN(opcode, program_counter, stack, stack_pointer); break;
+        case 0x3000:
+            Opcode::execute_3XNN(opcode, program_counter, V); break;
+        case 0x4000:
+            Opcode::execute_4XNN(opcode, program_counter, V); break;
+        case 0x5000:
+            Opcode::execute_5XY0(opcode, program_counter,V); break;
+        case 0x6000:
+            Opcode::execute_6XKK(opcode, program_counter, V); break;
+        case 0x7000:
+            Opcode::execute_7XKK(opcode, program_counter, V); break;
+        case 0x8000:
+            switch (opcode & 0x000F){
+                case 0:
+                    Opcode::execute_8XY0(opcode, program_counter, V); break;
+                case 1:
+                    Opcode::execute_8XY1(opcode, program_counter, V); break;
+                case 2:
+                    Opcode::execute_8XY2(opcode, program_counter, V); break;
+                case 3:
+                    Opcode::execute_8XY3(opcode, program_counter, V); break;
+                case 4:
+                    Opcode::execute_8XY4(opcode, program_counter, V); break;
+                case 5:
+                    Opcode::execute_8XY5(opcode, program_counter, V); break;
+                case 6:
+                    Opcode::execute_8XY6(opcode, program_counter, V); break;
+                case 7:
+                    Opcode::execute_8XY7(opcode, program_counter, V); break;
+                case E:
+                    Opcode::execute_8XYE(opcode, program_counter, V); break;
+                default:
+                    cerr << "No such opcode " + opcode << endl;
+            } break;
+        case 0x9000:
+            Opcode::execute_9XY0(opcode, program_counter, V); break;
+        case 0xA000:
+            Opcode::execute_ANNN(opcode, program_counter, I); break;
+        case 0xB000:
+            Opcode::execute_BNNN(opcode, program_counter, V[0]); break;
+        case 0xC000:
+            Opcode::execute_CXKK(opcode, program_counter, V); break;
+        case 0xD000:
+            Opcode::execute_DXYN(opcode, program_counter, memory, V, I, graphics); break;
+        case 0xE000:
+            switch(opcode & 0x00FF){
+                case 0x9E:
+                    Opcode::execute_EX9E(opcode, program_counter, V, keys); break;
+                case 0xA1:
+                    Opcode::execute_EXA1(opcode, program_counter, V, keys); break;
+                default:
+                    cerr << "No such opcode " + opcode << endl;
+            } break;
+        case 0xF000:
+            switch(opcode & 0x00FF){
+                case 0x07:
+                    Opcode::execute_FX07(opcode, program_counter, V, delay_timer); break;
+                case 0x0A:
+                    Opcode::execute_FX0A(opcode, program_counter, V, keys); break;
+                case 0x15:
+                    Opcode::execute_FX15(opcode, program_counter, V, delay_timer); break;
+                case 0x18:
+                    Opcode::execute_FX18(opcode, program_counter, V, sound_timer); break;
+                case 0x1E:
+                    Opcode::execute_FX1E(opcode, program_counter, V, I); break;
+                case 0x29:
+                    Opcode::execute_FX29(opcode, program_counter, V, I); break;
+                case 0x33:
+                    Opcode::execute_FX33(opcode, program_counter, V, I, memory); break;
+                case 0x55:
+                    Opcode::execute_FX55(opcode, program_counter, V, I, memory); break;
+                case 0x65:
+                    Opcode::execute_FX65(opcode, program_counter, V, I, memory); break;
+                default:
+                    cerr << "No such opcode " + opcode << endl;
+            } break;
+        default:
+            cerr << "No such opcode " + opcode << endl;
     }
-
-
-
-
-
 
     // Update timers
     if(delay_timer > 0){
@@ -81,7 +136,7 @@ void CHIP8::emulate_cycle(){
         if(sound_timer == 1){
             printf("BEEP!\n");}
         --sound_timer;
-    }*/
+    }
 }
 
 void CHIP8::load_program(const string file_path){
