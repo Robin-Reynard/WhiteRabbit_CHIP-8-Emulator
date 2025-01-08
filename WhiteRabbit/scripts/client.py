6b1fbdfc64ef0abbe1b9c9fc389925147940fba9
Robin-Reynard <robin.reynard.the.first@gmail.com>
2025-01-08 16:35:49 -0500

    Receive and display chip8 screenshot in script

--- WhiteRabbit/scripts/client.py
+++ WhiteRabbit/scripts/client.py
@@ -1,11 +1,13 @@
 import socket
 import sys
+import numpy as np
+from PIL import Image
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.0.143', 43985)
+server_address = ('192.168.0.143', 40265)
 print(sys.stderr, 'connecting to %s port %s' % server_address)
 sock.connect(server_address)
 data = 'This is the message.  It will be repeated.'
@@ -14,29 +16,56 @@ len_data = len(data)
 def try_send_data():
     print(sys.stderr, 'sending "%s"' % data)
     sock.sendall(data.encode())
-def try_receive_data():
+def try_receive_data(amount_expected=len_data):
     # Look for the response
     amount_received = 0
-    amount_expected = len_data
     
     while amount_received < amount_expected:
         data = sock.recv(len_data)
         amount_received += len_data
         print(sys.stderr, 'received "%s"' % data)
+def try_receive_image():
+    # Look for the response
+    amount_received = 0
+    image_data = bytearray(b'')
+    
+    while amount_received < 2048:
+        data = sock.recv(len_data)
+        amount_received += len_data
+        image_data.extend(data)
+        #print(image_data)
+        #print(sys.stderr, 'received "%s"' % data)
+    return image_data
 
 try:
-    data = "Hello There"
+    data = '{"request": "GET_IMAGE"}'
+    try_send_data()
+    received_image = try_receive_image()
+
+    print(received_image)
+
+    pixels = np.frombuffer(received_image, dtype=np.uint8).reshape((32, 64))
+    pixels[pixels > 0.5] = 255
+    print(pixels)
+    #pixels2 = np.where(pixels<1,0,255)
+    image = Image.fromarray(pixels)
+    image.show()
+
+    data = '{"request": "GET_CHIP8_INFO"}'
     try_send_data()
     try_receive_data()
 
-    data = "General Kenobi"
+    data = '{"request": "PUBLISH_COMMAND", "command_keystroke": "A", "command_duration":"5"}'
     try_send_data()
     try_receive_data()
 
-    data = ":D"
+    data = '{"request": "PUBLISH_TEXT","text": "Hello there!"}'
     try_send_data()
     try_receive_data()
 
+    data = '{"request": "PUBLISH_IMAGE","image": "Hello there!"}'
+    try_send_data()
+    try_receive_data()
     
 
 finally:
