eeb0992779d727d1067918b9a8d8fd0ce7b5489f
Robin-Reynard <robin.reynard.the.first@gmail.com>
2025-01-22 23:52:42 -0500

    client done fr

--- WhiteRabbit/scripts/client.py
+++ WhiteRabbit/scripts/client.py
@@ -1,118 +1,76 @@
-import io
-import socket
-import sys
 import time
 import numpy as np
 from PIL import Image
 import base64
 from io import BytesIO
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QByteArray, QDataStream, QIODevice, QTimer
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtNetwork import QTcpSocket
 
 class DemoClient():
     CHIP8_IMAGE_FORMAT = (32, 64)
+    
     def __init__(self, ip, port):
-
         self.socket = QTcpSocket()
-        self.connect_to_server(ip, port)
-        self.run_publish_text_command()
-        screen_capture = self.run_get_screen_capture_command()
-        self.run_publish_image_command(screen_capture)
-        self.run_publish_command_command()
-        self.close_port()
+        try:
+            self.connect_to_server(ip, port)
+            # Run demo commands
+            self.run_publish_text_command()
+            screen_capture = self.run_get_screen_capture_command(show_image=False)
+            self.run_publish_image_command(screen_capture)
+            self.run_publish_command_press()
+            self.run_get_chip8_info_command()
+            self.run_publish_command_release()
+        finally:
+            # Always properly close port
+            self.close_port()
 
-
-    def connect_to_server(self, ip, port):
-        # Reach out to server for connection
-        self.socket.connectToHost(ip, port)
-
-        # Wait up to 5 seconds for server to establish connection
-        self.wait_for_server_connection(5000)
+    ########################### SERVER REQUEST EXAMPLES ###########################
     
     def run_publish_text_command(self):
         request = '{"request": "PUBLISH_TEXT","text": "Hello there!"}'
-        self.socket.write(request.encode())
-        self.socket.flush()
-
-        print("PUBLISH_TEXT request sent! Waiting for server response")
-        self.wait_for_server_response(1000)
-
-        # Read the response from the server
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
+        message = "PUBLISH_TEXT request sent! Awaiting confirmation :3"
+        self.send_request(request, message)
     
-    def run_get_screen_capture_command(self):
+    def run_publish_command_press(self):
+        request = '{"request": "PUBLISH_COMMAND","command_key": "A","command_action": "PRESS"}'
+        message = "PUBLISH_COMMAND PRESSED request sent! The rabbit vanished!"
+        self.send_request(request, message)
+        time.sleep(2.5)
+    
+    def run_publish_command_release(self):
+        request = '{"request": "PUBLISH_COMMAND","command_key": "A","command_action": "RELEASE"}'
+        message = "PUBLISH_COMMAND RELEASE request sent! The rabbit came back!"
+        self.send_request(request, message)
+    
+    def run_get_chip8_info_command(self):
+        request = '{"request": "GET_CHIP8_INFO"}'
+        message = "GET_CHIP8_INFO request sent!"
+        self.send_request(request, message)
+    
+    def run_get_screen_capture_command(self, show_image=False):
         request = '{"request": "GET_SCREEN_CAPTURE"}'
-        self.socket.write(request.encode())
-        self.socket.flush()
-
-        print("GET_SCREEN_CAPTURE request sent! Waiting for server response to display image")
-        self.wait_for_server_response(1000)
-
-        # Get the image from the server
-        response = self.socket.readAll()
-        pixels = np.frombuffer(response, dtype=np.uint8).reshape(self.CHIP8_IMAGE_FORMAT)
-
-        # Display the image
-        pixels[pixels == 1] = 255
-        image = Image.fromarray(pixels)
-        image = image.resize((640,320))
-        image.show()
-
+        message = "GET_SCREEN_CAPTURE request sent! Waiting for server response to display image"
+        image = self.send_get_image_request(request, message)
+        if show_image:
+            image.show()
         return image
     
     def run_publish_image_command(self, screen_capture):
-        # Convert PIL Image to a base64 string
+        # Convert Image object to a base64 string for data transfer
         buffer = BytesIO()
         screen_capture.save(buffer, format="JPEG")
         img_str = base64.b64encode(buffer.getvalue()).decode()
 
         request = '{"request": "PUBLISH_IMAGE","image": " ' + img_str + '"}'
-        self.socket.write(request.encode())
-        self.socket.flush()
-
-        print("PUBLISH_IMAGE request sent! Your image should be displayed in WhiteRabbit!")
-        self.wait_for_server_response(1000)
-
-        # Read the response from the server
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
+        message = "PUBLISH_IMAGE request sent! Have a look-see in WhiteRabbit"
+        self.send_request(request, message)
     
-    def run_publish_command_command(self):
-        request = '{"request": "PUBLISH_COMMAND","command_key": "A","command_action": "PRESS"}'
-        self.socket.write(request.encode())
-        self.socket.flush()
-        print("PUBLISH_COMMAND PRESSED request sent! White Rabbit should be winking")
-        self.wait_for_server_response(10000)
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
-
-        request = '{"request": "PUBLISH_COMMAND","command_key": "A","command_action": "RELEASE"}'
-        self.socket.write(request.encode())
-        self.socket.flush()
-        print("PUBLISH_COMMAND RELEASE request sent! White Rabbit should be looking at you")
-        self.wait_for_server_response(10000)
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
-
-        
-        time.sleep(5)
-
-        print("PUBLISH_COMMAND PRESSED request sent! White Rabbit should be winking")
-        self.wait_for_server_response(10000)
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
-        print("PUBLISH_COMMAND RELEASE request sent! White Rabbit should be looking at you")
-        self.wait_for_server_response(10000)
-        response = self.socket.readAll().data().decode()
-        print("    Server says", response)
-
-        
-
-
-
-
+    ########################### SERVER COMMUNICATION METHODS ###########################
+    def connect_to_server(self, ip, port):
+        # Reach out to server for connection
+        self.socket.connectToHost(ip, port)
+        # Wait up to 5 seconds for server to establish connection
+        self.wait_for_server_connection(5000)
 
     def wait_for_server_connection(self, time_in_ms):
         if not self.socket.waitForConnected(time_in_ms):
@@ -125,96 +83,41 @@ class DemoClient():
         if not self.socket.waitForReadyRead(time_in_ms):
             print("Failed to receive data:", self.socket.errorString())
             exit(1)
+
+    def send_request(self, request, message):
+        # Send data to server
+        self.socket.write(request.encode())
+        self.socket.flush()
+        print(message)
+
+        # Display server answer when available
+        self.wait_for_server_response(1000)
+        response = self.socket.readAll().data().decode()
+        print("  [Server responded]", response)
     
+    def send_get_image_request(self, request, message):
+        # Send data to server
+        self.socket.write(request.encode())
+        self.socket.flush()
+        print(message)
+
+        # Get the image from the server in the form of QByteArray when available
+        self.wait_for_server_response(1000)
+        response = self.socket.readAll()
+        # Convert QByteArray to matrix
+        pixels = np.frombuffer(response, dtype=np.uint8).reshape(self.CHIP8_IMAGE_FORMAT)
+        # Increase the contrast between black/white
+        pixels *= 255
+        # Create image object 
+        image = Image.fromarray(pixels)
+        # Imcrease image size
+        image = image.resize((640,320))
+
+        return image
+
     def close_port(self):
         self.socket.disconnectFromHost()
 
 
 app = QApplication([])
 client = DemoClient('127.0.0.1', 3000)
-#client.start_on_connected()
-#app.exec()
-
-
-'''
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#Qt_5_12
-
-# Connect the socket to the port where the server is listening
-server_address = ('127.0.0.1', 3000)
-print(sys.stderr, 'connecting to %s port %s' % server_address)
-sock.connect(server_address)
-data = 'This is the message.  It will be repeated.'
-len_data = len(data)
-
-def try_send_data():
-    print(sys.stderr, 'sending "%s"' % data)
-    sock.sendall(data.encode())
-def try_receive_data(amount_expected=len_data):
-    # Look for the response
-    amount_received = 0
-    
-    while amount_received < amount_expected:
-        data = sock.recv(len_data)
-        amount_received += len_data
-        print(sys.stderr, 'received "%s"' % data)
-
-def try_receive_image():
-    # Look for the response
-    amount_received = 0
-    image_data = bytearray(b'')
-    
-    while amount_received < 2048:
-        data = sock.recv(len_data)
-        amount_received += len_data
-        image_data.extend(data)
-    return image_data
-
-try:
-    data = '{"request": "GET_SCREEN_CAPTURE"}'
-    try_send_data()
-    received_image = try_receive_image()
-
-    print(received_image)
-
-    pixels = np.frombuffer(received_image, dtype=np.uint8).reshape((32, 64))
-    pixels[pixels > 0.5] = 255
-    print(pixels)
-    #pixels2 = np.where(pixels<1,0,255)
-    image = Image.fromarray(pixels)
-    image = image.resize((640,320))
-    image.show()
-    
-    data = '{"request": "PUBLISH_COMMAND","command_key": "B","command_action": "PRESS"}'
-    try_send_data()
-    try_receive_data()
-
-
-    data = '{"request": "GET_CHIP8_INFO"}'
-    try_send_data()
-    try_receive_data(1000)
-
-    
-    data = '{"request": "PUBLISH_COMMAND","command_key": "A","command_action": "RELEASE"}'
-    try_send_data()
-    try_receive_data()
-
-    data = '{"request": "PUBLISH_TEXT","text": "Hello there!"}'
-    try_send_data()
-    try_receive_data()
-
-    buff = BytesIO()
-    image.save(buff, format="JPEG")
-    img_str = base64.b64encode(buff.getvalue()).decode()
-    print(img_str)
-    data = '{"request": "PUBLISH_IMAGE","image": " ' + img_str + '"}'
-    try_send_data()
-    try_receive_data()
-    
-
-finally:
-    print(sys.stderr, 'closing socket')
-    sock.close()'''
